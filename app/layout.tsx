de893c5af44e2df6cacb72fd092ee8e70d9d1079
Juan Serrano <Juanfater2013@hotmail.com>
2025-04-21 20:44:26 -0500

    feat: update metadata for improved SEO and change project name to MultiGames
    Signed-off-by: Juan Serrano <Juanfater2013@hotmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,27 +3,36 @@ import "@/app/globals.css"
 import {Inter} from "next/font/google"
 import {ThemeProvider} from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata = {
     title: "MultiGims - Comunidad de Desarrollo Tecnológico",
-  description:
-    "Una comunidad de profesionales tech en un servidor Discord donde colaboramos en proyectos de desarrollo web, móvil, gaming, IA y más.",
-    generator: 'v0.dev'
+    description: "Una comunidad de profesionales tech en un servidor Discord donde colaboramos en proyectos de desarrollo web, móvil, gaming, IA y más.",
+    twitter: {
+        card: 'summary_large_image',
+        title: 'MultiGims - Comunidad de Desarrollo Tecnológico',
+        description: 'Una comunidad de profesionales tech en un servidor Discord donde colaboramos en proyectos de desarrollo web, móvil, gaming, IA y más.',
+        images: ['/portada2.jpg'],
+    },
+    openGraph: {
+        title: 'MultiGims - Comunidad de Desarrollo Tecnológico',
+        description: 'Una comunidad de profesionales tech en un servidor Discord donde colaboramos en proyectos de desarrollo web, móvil, gaming, IA y más.',
+        images: ['/portada2.jpg'],
+    }
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="es" suppressHydrationWarning>
-      <body className={inter.className}>
+    return (
+        <html lang="es" suppressHydrationWarning>
+        <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
+            {children}
         </ThemeProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
